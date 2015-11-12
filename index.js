97e6936651dd42beb0af1d24a7ac2d983713caff
James Thomas <james.thomas@uk.ibm.com>
2015-11-12 15:41:31 +0000

    Re-factoring.

--- index.js
+++ index.js
@@ -1,22 +1,26 @@
-const CFBot = require('./lib/cf_bot')
-const cfenv = require("cfenv")
+'use strict'
 
-var credentials = cfenv.getAppEnv().getServiceCreds('cfbot')
+const CFBot = require('./lib/cf_bot')
+const cfenv = require('cfenv')
+const winston = require('winston')
+
+const credentials = cfenv.getAppEnv().getServiceCreds('cfbot')
+winston.level = process.env.LOG_LEVEL || 'info'
 
 if (!credentials) {
-  console.error('ERROR: Missing service credentials for cfbot.\nHave you configured and bound them to your application? Exiting...')
+  winston.error('Missing service credentials for cfbot. Have you configured and bound them to your application? Exiting...')
   process.exit(1)
 }
 
-var parameters = ['slack_token', 'cf_api', 'cf_username', 'cf_password']
-
 var check_missing_param = function (param) {
   if (!credentials[param]) {
-    console.error(`ERROR: Invalid service credentials, missing the ${param} parameter. Exiting...`)
+    winston.error(`Invalid service credentials, missing the ${param} parameter. Exiting...`)
     process.exit(1)
   }
 }
 
+var parameters = ['slack_token', 'cf_api', 'cf_username', 'cf_password']
 parameters.forEach(check_missing_param)
 
+winston.info('Ahoy! Attempting to start CF Bot... ')
 var cf_bot = new CFBot(credentials.slack_token, credentials.cf_api, {username: credentials.cf_username, password: credentials.cf_password})
